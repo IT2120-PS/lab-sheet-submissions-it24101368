b7a126083ca2865b13d40636eae029f00e2bbea0
it24101368 <it24101368@my.sliit.lk>
2025-09-18 15:57:56 +0530

    Lab6

---
+++ Lab Sheet 06/Lab6.R
@@ -0,0 +1,23 @@
+setwd('C:\\Users\\LOQ\\Desktop\\IT24101368 Lab6\\Lab 06-20250918')
+getwd()
+
+##Part-1
+#i)
+#Binomial Distribution
+
+#ii)
+#P(X>=47) = 1-P(X<=46)
+1-pbinom(46,50,0.85,TRUE)
+
+##Part-2
+#i)
+#X= Number of calls received in one hour
+
+#ii)
+#Poisson Distribution
+
+
+#iii)
+#X=15
+dpois(15,12)
+
